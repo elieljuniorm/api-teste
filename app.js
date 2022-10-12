dbaa2f143d9bea824185fb9d881e8951baeae021
Eliel Maia <elieljuniorm@gmail.com>
2022-10-12 14:41:59 -0300

    configurando json e validando usuario criado

--- app.js
+++ app.js
@@ -8,9 +8,23 @@ const res = require('express/lib/response')
 
 const app = express()
 
+//Cofiguração JSON response (sem isso o JSON não é lido)
+app.use(express.json())
+
 // Open Route - Public Route
 app.get('/', (req, res) => {
     res.status(200).json({ msg: 'Bem vindo a API!' })
+})
+
+//Registrar usuário
+
+app.post('/auth/register', async (req, res) => {
+    const { name, email, password, confirmpassword } = req.body
+
+    //Validação
+    if (!name) {
+        return res.status(422).json({ msg: "O nome é obrigatório" })
+    }
 })
 
 //Credenciais
@@ -19,8 +33,8 @@ const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASS
 
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.pbrb9mh.mongodb.net/?retryWrites=true&w=majority`)
-.then(() => {
-    app.listen(3000)
-    console.log("Conectou ao banco")
-})
-.catch((err) => console.log(err))
+    .then(() => {
+        app.listen(3000)
+        console.log("Conectou ao banco")
+    })
+    .catch((err) => console.log(err))
