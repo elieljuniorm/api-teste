3f74adb12dff540fb5c4b276de81ccc2724af4f3
Eliel Maia <elieljuniorm@gmail.com>
2022-10-12 17:28:58 -0300

    api finalizada

--- app.js
+++ app.js
@@ -19,6 +19,43 @@ app.get('/', (req, res) => {
     res.status(200).json({ msg: 'Bem vindo a API!' })
 })
 
+//Private Route (checkToken)
+app.get('/user/:id', checkToken, async (req, res) => {
+
+    const id = req.params.id
+
+    //Checar usuário existe
+    const user = await User.findById(id, '-password')
+
+    if (!user) {
+        return res.status(404).json({ msg: "Usuário não encontrado!" })
+    }
+
+    res.status(200).json({ user })
+})
+
+function checkToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if (!token) {
+        return res.status(401).json({ msg: "Acesso negado!" })
+    }
+
+    try {
+
+        const secret = process.env.SECRET
+
+        jwt.verify(token, secret)
+
+        next()
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ msg: "Token inválido" })
+    }
+}
+
 //Registrar usuário
 
 app.post('/auth/register', async (req, res) => {
